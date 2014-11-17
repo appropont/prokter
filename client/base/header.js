832152cfd845e6894a4971586df56cdf56ac12c4
Chris Griffing <cmgriffing@gmail.com>
2014-11-17 05:20:20 -0800

    fixed hiding of mobile menu on menu item click to only fire if actually showing mobile menu

--- client/base/header.js
+++ client/base/header.js
@@ -2,7 +2,8 @@ Template.header.events({
 
     //This hides the mobile menu on click
     'click #navbar-collapse li a' : function(e) {
-        $('#navbar-toggle').click();
+        if($('#navbar-collapse').hasClass('in'))
+            $('#navbar-toggle').click();
     }
 
 })
